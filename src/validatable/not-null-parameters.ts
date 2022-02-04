a7bef3583480bd117c038eb9c785ac9c9d39e591
dikac <rigel234@yahoo.com>
2022-02-05 01:33:45 +0700

    change type for return to return

--- src/validatable/not-null-parameters.ts
+++ src/validatable/not-null-parameters.ts
@@ -5,23 +5,23 @@ import NotNullMessage from "../assert/string/not-null-parameters";
 import Static from "@alirya/validator/message/function/static-parameters";
 import ValidatableType from "@alirya/validator/validatable/validatable";
 
-export type NotNullParametersType<Argument, MessageType> = Readonly<ValidatableStatic<Argument, null, true, false, ValidatableType<Argument | null, MessageType>>>;
+export type NotNullParametersReturn<Argument, MessageType> = Readonly<ValidatableStatic<Argument, null, true, false, ValidatableType<Argument | null, MessageType>>>;
 
 export default function NotNullParameters<Argument>(
     value : Argument
-) : NotNullParametersType<Argument, string>;
+) : NotNullParametersReturn<Argument, string>;
 
 export default function NotNullParameters<Argument, MessageType>(
     value : Argument,
     message : Static<Argument, null, true, false, MessageType>
-) : NotNullParametersType<Argument, MessageType>;
+) : NotNullParametersReturn<Argument, MessageType>;
 
 export default function NotNullParameters<Argument, MessageType>(
     value : Argument,
     message : Static<Argument, null, true, false, MessageType|string> = NotNullMessage
-) : NotNullParametersType<Argument, MessageType> {
+) : NotNullParametersReturn<Argument, MessageType> {
 
-    return <NotNullParametersType<Argument, MessageType>> Callback(value, NotNullBoolean, message);
+    return <NotNullParametersReturn<Argument, MessageType>> Callback(value, NotNullBoolean, message);
 }
 
 
